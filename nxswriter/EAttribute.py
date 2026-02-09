d8c0be45b9299d29b8aa1bcada0fa794cba761b7
Jan Kotanski <jankotan@gmail.com>
2026-02-09 16:15:15 +0100

    store strategy and datasource name of attributes in writer modules

--- nxswriter/EAttribute.py
+++ nxswriter/EAttribute.py
@@ -153,6 +153,11 @@ class EAttribute(FElement):
                                     NTP.nTnp[
                                         self.last.tagAttributes[self.name][0]],
                                     vl.shape, overwrite=True)
+                        if self.source and self.source._name:
+                            if hasattr(self.h5Object, "_dsname"):
+                                self.h5Object._dsname = self.source._name
+                            if hasattr(self.h5Object, "_strategy"):
+                                self.h5Object._strategy = self.strategy
                         self.h5Object[...] = dh.cast(self.h5Object.dtype)
 
         except Exception:
--- nxswriter/Release.py
+++ nxswriter/Release.py
@@ -21,4 +21,4 @@
 
 
 #: package version
-__version__ = "3.19.0"
+__version__ = "3.20.0"
