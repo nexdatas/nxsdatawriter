658bcc72efea08a7fb491230b08b4bcb78f7adf4
Jan Kotanski <jankotan@gmail.com>
2026-09-10 08:08:49 +0200

    simplify pixi tests

--- .ci/run_pixi.sh
+++ .ci/run_pixi.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
+
 echo "install pixi"
-docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; pixi shell-hook  --manifest-path .github/workflows/pixi/pixi.toml > .sh.sh ; source .sh.sh ; pixi add  --manifest-path .github/workflows/pixi/pixi.toml rattler-build'
-# docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; pixi shell-hook  --manifest-path .github/workflows/pixi/pixi.toml > .sh.sh ; source .sh.sh ; pixi add  --manifest-path .github/workflows/pixi/pixi.toml rattler-build nxsconfigserver-db'
-# # /home/tango
-# docker exec  ndts /bin/bash -c 'pwd '
-# # /var/lib/tango
-# docker exec  ndts /bin/bash -c 'echo "$HOME" '
+docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; cp .github/workflows/pixi/pixi.toml . ; pixi shell-hook  > .sh.sh ; source .sh.sh ; pixi add   numpy "pytango=10.1.4" setuptools pip wheel argcomplete lxml pytz pyyaml  python-dateutil pninexus fabio h5py matplotlib-base blissdata pytest docutils nxsconfigserver pymysql nxstools pymysql'
 
-# # create nxsconfig database
-# docker exec  --user root ndts /bin/bash -c 'source /home/tango/.sh.sh ; export MYSQL_PASSWORD="rootpw" ; create_nxsconfig_db -x -d=nxsonfig -u=tango -p="$CONDA_PREFIX"'
+echo "run nxswriter tests"
+docker exec  ndts /bin/bash -c 'source .sh.sh ;  echo "export MYTANGO_PREFIX=$CONDA_PREFIX/bin" > /home/tango/.env ;   python -m pip install . -vv --no-deps --no-build-isolation ; python -m pytest'
 
-echo "run nxsconfigserver-db"
-docker exec  ndts /bin/bash -c 'source .sh.sh ; pixi run  --manifest-path .github/workflows/pixi/pixi.toml rattler-build build  --recipe .github/workflows/pixi/recipe.yaml'
 
 ERROR=$?
 if [ $ERROR -ne "0" ]
